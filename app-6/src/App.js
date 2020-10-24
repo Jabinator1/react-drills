67f95aba8377256d71729084708cfa4e69ddf290
Joesh <joesh@Jacobs-MBP-2.domain>
2020-10-24 17:17:24 -0600

    finished the assignment

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -5,20 +5,35 @@ class App extends Component {
   constructor(){
     super()
     this.state = {
-      todoList: []
+      todoList: [],
+      word: ""
     }
   }
 
-  inputChange(e) {
-
+  inputChange = letter => {
+    this.setState({
+      word: letter
+    })
   }
 
+  onClick = () => {
+    this.setState({
+      todoList: [...this.state.todoList, this.state.word],
+      word: ""
+    })
+  }
   render() {
+
+    const list = this.state.todoList.map(item => {
+      return <p>{item}</p>
+    })
+
     return (
       <div className="App">
         <h1>My to-do list:</h1>
         <input onChange={e => this.inputChange(e.target.value)} placeholder="Enter a new task" type="text"/>
-        <button>Add</button>
+        <button onClick={this.onClick}>Add</button>
+        {list}
       </div>
     )
   }
